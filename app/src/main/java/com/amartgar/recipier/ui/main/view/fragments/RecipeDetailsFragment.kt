b0d0e9aa1f00653e9de6dec3c88f37b9a305a662
Andrew Martgar <andrewmartgar@gmail.com>
2021-04-05 19:18:37 +0200

    Palette feature added. Recipe title backgrounds now change color.

--- app/src/main/java/com/amartgar/recipier/ui/main/view/fragments/RecipeDetailsFragment.kt
+++ app/src/main/java/com/amartgar/recipier/ui/main/view/fragments/RecipeDetailsFragment.kt
@@ -1,14 +1,23 @@
 package com.amartgar.recipier.ui.main.view.fragments
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.graphics.drawable.toBitmap
+import androidx.core.graphics.drawable.toDrawable
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import androidx.palette.graphics.Palette
 import com.amartgar.recipier.databinding.FragmentRecipeDetailsBinding
 import com.amartgar.recipier.ui.main.view.activities.MainActivity
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import java.io.IOException
 import java.util.*
 
@@ -37,6 +46,36 @@ class RecipeDetailsFragment : Fragment() {
                 Glide.with(requireActivity())
                     .load(it.recipeDetails.image)
                     .centerCrop()
+                    .listener(object : RequestListener<Drawable> {
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            Log.e("TAG", "Error loading image")
+                            return false
+                        }
+
+                        override fun onResourceReady(
+                            resource: Drawable?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            dataSource: DataSource?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            resource?.let {
+                                Palette.from(resource.toBitmap())
+                                    .generate() { palette ->
+                                        val intColor = palette?.lightVibrantSwatch?.rgb ?: 0
+                                        // (requireActivity() as MainActivity).supportActionBar?.setBackgroundDrawable(intColor.toDrawable())
+                                        mBinding!!.tvRecipeTitle.setBackgroundColor(intColor)
+                                    }
+                            }
+                            return false
+                        }
+
+                    })
                     .into(mBinding!!.ivRecipeImage)
             } catch (e: IOException) {
                 e.printStackTrace()
@@ -49,6 +88,7 @@ class RecipeDetailsFragment : Fragment() {
             mBinding!!.tvRecipeDirectionsText.text = it.recipeDetails.cookingDirection
             mBinding!!.tvRecipeTime.text = it.recipeDetails.cookingTime
             (requireActivity() as MainActivity).supportActionBar?.title = it.recipeDetails.title
+
 
         }
     }
